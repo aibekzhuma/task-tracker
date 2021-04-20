e36b657b32960aa14fd1c8cd866ac53cf63d1359
Aibek Zhumabekov <aibekzhumabekov@Aibeks-MBP.socal.rr.com>
2021-04-19 21:39:44 -0700

    added styles with delete func

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from 'react'
+import Header from './components/Header';
+import Tasks from './components/Tasks'
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+    id: 24,
+    text: "FoodNetwork",
+    day: 'Feb 5th at 2:40'
+    },
+        {
+    id: 4,
+    text: "FoodNetwork",
+    day: 'Feb 5th at 2:40'
+    },
+    {
+    id: 14,
+    text: "FoodNetwork",
+    day: 'Feb 5th at 2:40'
+    }
+])
+
+const deleteTask = (id)=>{
+  setTasks(tasks.filter((task)=>task.id !== id))
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <Header />
+        <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,18 @@
+
+import Task from './Task'
+const Tasks = ({tasks, onDelete}) => {
+
+    return (
+        <>
+        {tasks.map((task)=>{
+           return <Task key={task.id} task={task}
+           onDelete={()=>onDelete(task.id)}
+           />
+
+        })}
+            
+        </>
+    )
+}
+
+export default Tasks
